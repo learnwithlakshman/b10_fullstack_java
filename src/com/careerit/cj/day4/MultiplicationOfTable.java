40ec5e666b2081919e304979bd122dd062d83050
avulalakshman <learnwithlakshman@gmail.com>
2021-09-03 07:57:47 +0530

    Added day4 programs

---
+++ src/com/careerit/cj/day4/MultiplicationOfTable.java
@@ -0,0 +1,13 @@
+package com.careerit.cj.day4;
+
+public class MultiplicationOfTable {
+
+		public static void main(String[] args) {
+			
+				int num = 19;
+				
+				for(int i=1;i<=10;i++) {
+					System.out.println(num+" * "+i+" = "+(num*i));
+				}
+		}
+}
