2af8d218df666a27faecd5a32ef48f1ed8fdf4a8
avulalakshman <learnwithlakshman@gmail.com>
2021-11-10 08:14:30 +0530

    Added assignment questions and js example

---
+++ ui-ws/js-ws/js/main.js
@@ -0,0 +1,17 @@
+function showDate(){
+    document.getElementById('idDate').innerHTML=`<h3> Today date and time :${new Date()}</h3>`;
+}
+
+function showTable() {
+    let strNum = document.querySelector("#num").value;
+    if (strNum.trim().length > 0) {
+        let num = Number.parseInt(strNum);
+        let str = "";
+        for (let i = 1; i <= 10; i++) {
+            str += `${num} * ${i} = ${ num * i}<br/>`
+        }
+        document.querySelector("#idShowTable").innerHTML = str;
+    }else{
+        document.querySelector("#idShowTable").innerHTML = "";
+    }
+}
